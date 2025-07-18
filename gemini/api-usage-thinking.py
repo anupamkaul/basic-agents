62e6322f6dd5f6f2f7e0bbd14dfcba49b106460e
Anupam <anupam.kaul@yahoo.com>
2025-07-18 10:42:20 -0700

    Add thinking calls to gemini to get COT
    Added token budgets, add 2 responses (how-llms-work and impact of superman)
    Even though the answers are good, couldn't get gemini flash 2.5 to explain its thinking
    (this model is based on a 2024 version as apparent from the gemini-conversation.txt chat I had
    with it)

---
+++ gemini/api-usage-thinking.py
@@ -0,0 +1,59 @@
+from google import genai
+from google.genai import types
+key_path = '/Users/anupkaul/.key/GEMINI-KEY.txt' 
+
+try:
+    with open(key_path, 'r', encoding='utf-8') as file:
+        GEMINI_API_KEY = file.read()
+        GEMINI_API_KEY = GEMINI_API_KEY.rstrip('\n')
+
+    print("File content read successfully:")
+    #print("My GEMINI Key: ", GEMINI_API_KEY)
+except FileNotFoundError:
+    print(f"Error: The file '{file_path}' was not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# the client gets the AI key from the env variable 'GEMINI_API_KEY' (with no arguements)
+# but here we do https://ai.google.dev/gemini-api/docs/api-key#provide-api-key-explicitly
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+print(client)
+
+# usage examples
+
+# text generation
+# https://ai.google.dev/gemini-api/docs/text-generation
+
+# for a thinking budget see https://ai.google.dev/gemini-api/docs/thinking#set-budget 
+
+while True:
+
+    try:
+        user_contents = input("\nquery: ")
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+
+            #contents="What is the latest happening on lung cancer research?"
+            contents=user_contents,
+
+            config=types.GenerateContentConfig(
+                #thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+                thinking_config=types.ThinkingConfig(thinking_budget=-1) # Enables thinking (0 to 24576 tokens, gemini 2.5 flash)
+            ),
+        )
+
+        print(response.text)
+
+    except KeyboardInterrupt:
+        print("\nInterrupt detected, exiting loop.")
+        break
+
+
+
+
+
+
+
+
