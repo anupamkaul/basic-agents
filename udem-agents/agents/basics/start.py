9ee671fabe0272e3ced14b6809d19d2d523f3a07
Anupam <anupam.kaul@yahoo.com>
2025-12-07 16:42:05 -0800

    Add openAPI paidkey and run openAI (2 models) : answer their own questions

--- udem-agents/agents/basics/start.py
+++ udem-agents/agents/basics/start.py
@@ -21,13 +21,38 @@ print(openai) # just a pointer, not that it matters much
 # create a list of messages in familiar openAI format
 
 my_messages = [{"role": "user", "content": "what is 2+3"}]
-
 response = openai.chat.completions.create(
             model="gpt-4.1-nano",
             messages=my_messages
         )
+print(response.choices[0].message.content)
 
-print(response)
+# now let's change the model to a cheaper one, ask it to ask a difficult question
+# and then have it answer its question itself:
+
+question = "Please propose a hard, challenging question to assess someone's IQ. Respond only with the question."
+my_messages = [{"role": "user", "content": question}]
+
+# ask it - this uses GPT 4.1 mini, still cheap but more powerful than nano
+response = openai.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=my_messages
+)
+
+question = response.choices[0].message.content
+print(question)
+
+# now let it answer its own hard question :) 
+my_messages = [{"role": "user", "content": question}]
+response = openai.chat.completions.create(
+    model="gpt-4.1-mini",
+    messages=my_messages
+)
+
+answer = response.choices[0].message.content
+print(answer)
+
+
 
 
 
